6c2d9b102dc88047b9b8a8c272c8584ccf6f4de7
Jeremy <103984916@student.swin.edu.au>
2024-08-12 19:38:52 +1000

    Fixed p1 issues

--- sub-tasks/Task 1/stock-prediction/stock_prediction.py
+++ sub-tasks/Task 1/stock-prediction/stock_prediction.py
@@ -147,7 +147,8 @@ def create_model(sequence_length, n_features, units=256, cell=LSTM, n_layers=2, 
             if bidirectional:
                 model.add(Bidirectional(cell(units, return_sequences=True), batch_input_shape=(None, sequence_length, n_features)))
             else:
-                model.add(cell(units, return_sequences=True, batch_input_shape=(None, sequence_length, n_features)))
+               model.add(cell(units, return_sequences=True, batch_input_shape=(None, sequence_length, n_features)))
+
         elif i == n_layers - 1:
             # last layer
             if bidirectional:
